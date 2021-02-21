988e3fe5f82f6b0a456f6638992f4f238eec184d
Anton Hoang <anton.hoang@nitrix.io>
2021-02-21 20:46:23 +0200

    initial commit

---
+++ The Movie DB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  The Movie DB
+//
+//  Created by Anton Hoang on 21.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
