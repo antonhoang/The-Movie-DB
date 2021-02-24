7d15c7b082440d2b9c5c5e074c6f03c2388891cb
Anton Hoang <anton.hoang@nitrix.io>
2021-02-24 22:43:31 +0200

    add modules

---
+++ The Movie DB/Model/Networking/RequestItem.swift
@@ -0,0 +1,14 @@
+//
+//  RequestItem.swift
+//  The Movie DB
+//
+//  Created by Anton Hoang on 22.02.2021.
+//
+
+import Foundation
+
+struct RequestItem {
+  let API_KEY = "a1e6469b9c841dbf821f4ef57f4d74f0" // v3 auth value // key - api_key
+  let TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMWU2NDY5YjljODQxZGJmODIxZjRlZjU3ZjRkNzRmMCIsInN1YiI6IjYwMzM1OGY5OWFmMTcxMDAzZjQ1YjNiNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.H-f-0iZNchiOoPmJc42k5p7vszMqsvOqd9K-6p1JiL0" //  Bearer token v4 auth
+}
+
