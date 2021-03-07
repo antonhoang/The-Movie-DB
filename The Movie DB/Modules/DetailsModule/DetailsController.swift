020d81ed98ae32413f7860049935e4efda667019
Anton Hoang <anton.hoang@nitrix.io>
2021-03-07 17:40:21 +0200

    add image container

--- The Movie DB/Modules/DetailsModule/DetailsController.swift
+++ The Movie DB/Modules/DetailsModule/DetailsController.swift
@@ -14,19 +14,24 @@ final class DetailsController: UIViewController {
   var coordinator: DetailsCoordinatorFlow?
   
   fileprivate let movieImageView: UIImageView = {
+    $0.backgroundColor = .systemGreen
     $0.contentMode = .scaleToFill
     return $0
   }(UIImageView())
   
   fileprivate let scrollView: UIScrollView = {
+    $0.backgroundColor = .systemTeal
     return $0
   }(UIScrollView())
   
   fileprivate let imageContainerView: UIView = {
+    $0.translatesAutoresizingMaskIntoConstraints = false
+    $0.backgroundColor = .systemBlue
     return $0
   }(UIView())
   
   fileprivate let contentView: UIView = {
+    $0.backgroundColor = .systemOrange
     return $0
   }(UIView())
 
@@ -38,9 +43,30 @@ final class DetailsController: UIViewController {
   fileprivate func setupUI() {
     navigationItem.title = "Details"
     view.backgroundColor = .systemGreen
+    setupScrollView()
+    setupImageContainerView()
+    setupContentBlockView()
   }
   
-  fileprivate func setupScrollView() { }
-  fileprivate func setupImageContainerView() { }
+  fileprivate func setupScrollView() {
+    view.addSubview(scrollView)
+    
+    scrollView.addSubview(imageContainerView)
+    scrollView.addSubview(contentView)
+    scrollView.addSubview(movieImageView)
+    
+    scrollView.fillSuperview()
+    
+  }
+  
+  fileprivate func setupImageContainerView() {
+    NSLayoutConstraint.activate([
+      imageContainerView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+      imageContainerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+      imageContainerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+      imageContainerView.heightAnchor.constraint(equalTo: imageContainerView.widthAnchor, multiplier: 0.7)
+    ])
+  }
+  
   fileprivate func setupContentBlockView() { }
 }
