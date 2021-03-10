bfea67911cef96c26b7217f4047589d3ab88fd2d
anton.hoang <anton.hoang@nitrix.io>
2021-03-10 18:31:13 +0200

    add king fisher

--- The Movie DB/Helpers/Extensions/UIImageView+AsyncLoadImage.swift
+++ The Movie DB/Helpers/Extensions/UIImageView+AsyncLoadImage.swift
@@ -8,31 +8,33 @@
 
 import Foundation
 import UIKit
+import Kingfisher
 
 extension UIImageView {
   func loadImage(imagePath: String) {
     guard let imageURL = URL(string: imagePath) else { return }
+    self.kf.setImage(with: imageURL)
     
-    var data: Data?
-    let queue = DispatchQueue.global(qos: .utility)
-    
-    let workItem = DispatchWorkItem {
-      do {
-        data = try Data(contentsOf: imageURL)
-      } catch let error {
-        print(error)
-      }
-    }
-    
-    queue.async(execute: workItem)
-    workItem.notify(queue: .main) {
-      if let imageData = data {
-        DispatchQueue.main.async {
-          UIView.transition(with: self, duration: 0.5, options: .transitionCrossDissolve) { [weak self] in
-            self?.image = UIImage(data: imageData)
-          }
-        }
-      }
-    }
+//    var data: Data?
+//    let queue = DispatchQueue.global(qos: .utility)
+//
+//    let workItem = DispatchWorkItem {
+//      do {
+//        data = try Data(contentsOf: imageURL)
+//      } catch let error {
+//        print(error)
+//      }
+//    }
+//
+//    queue.async(execute: workItem)
+//    workItem.notify(queue: .main) {
+//      if let imageData = data {
+//        DispatchQueue.main.async {
+//          UIView.transition(with: self, duration: 0.5, options: .transitionCrossDissolve) { [weak self] in
+//            self?.image = UIImage(data: imageData)
+//          }
+//        }
+//      }
+//    }
   }
 }
